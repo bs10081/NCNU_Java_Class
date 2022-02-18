525fbd52a4afd1a729d961588af8b9ec6372334f
bs10081 <bs10081@bs10081.com>
2022-02-18 17:06:30 +0800

    Base java input program.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Hello {
     public static void main(String[] argv) {
-        System.out.println("Hello World");
+        // byte, short, int, long, float, double, boolean, char
+        // int x;
+        // float y;
+        // x = 6;
+        // y = (float)6.1;
+        // x = (int)y;
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        System.out.println("Hello World, "+x);
     }
 }
