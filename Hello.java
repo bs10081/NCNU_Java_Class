3230175adfbef08ba85400fe10884d144258bbba
bs10081 <bs10081@bs10081.com>
2022-02-18 18:22:58 +0800

    Simple HelloWorld.

--- Hello.java
+++ Hello.java
@@ -1,12 +1,6 @@
 import java.util.Scanner;
 public class Hello {
     public static void main(String[] argv) {
-        // byte, short, int, long, float, double, boolean, char
-        // int x;
-        // float y;
-        // x = 6;
-        // y = (float)6.1;
-        // x = (int)y;
         Scanner input = new Scanner(System.in);
         int x = input.nextInt();
         System.out.println("Hello World, "+x);
