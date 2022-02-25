e86a6cc7eac2166e57c6d7761ba4cab5fd90a197
bs10081 <bs10081@bs10081.com>
2022-02-25 16:49:32 +0800

    Add 3n+1

---
+++ N3.java
@@ -0,0 +1,37 @@
+// 3n + 1 problem
+// input: two longs start and end
+// oputput: longest sequence length of all sequence between start and end
+import java.util.Scanner;
+public class N3 {
+    public static long seqLen(long n) {
+        long length = 1;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+            length = length + 1;
+        }
+        return length;
+    }
+    public static long maxN3(long start, long end) {
+        if (start > end) {
+            long tmp = start;
+            start = end;
+            end = tmp;
+        }
+        long maxLen = 0;
+        for (long i = start; i <= end; i = i+1) {
+            long len = seqLen(i);
+            if (len > maxLen) 
+                maxLen = len;
+        }
+        return maxLen;
+    }
+    public static void main(String[] argv) {
+        Scanner input = new Scanner(System.in);
+        long start = input.nextLong(), end=input.nextLong();
+        System.out.println(start+ " " + end + " " + maxN3(start, end));
+    }
+}
