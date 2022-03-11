3c02f49c5171d3a4039a1cb9a62e90705f295d96
bs10081 <bs10081@bs10081.com>
2022-03-11 18:03:38 +0800

    Add Quick Sort.

--- Sort.java
+++ Sort.java
@@ -45,9 +45,51 @@ public class Sort {
                 }
         }
     }
+    // quick sort --> divide and conquer
+    // key idea:
+    // divide an unsorted array by v, such that
+    // 小小小小小v大大大大大
+    // then sort 小 and 大 seperatedly
+    // sort elements between start and end
+
+    // Overload(only Java and C++...)
+    public static void quickSort(int[] data) {
+        quickSort(data, 0, data.length-1);
+    }
+    public static void quickSort(int[] data, int start, int end) {
+        // choose which v? simplest way is to choose datae[start]
+        // then left <-- start+1, right <-- end
+        // from left choose an element >= v
+        // from right choose and element <= v 
+        // swap left and right if left pos < right pos
+        if (start >= end)
+            return;
+        int v = data[start];    // choose leftest element
+        int left = start+1;
+        int right = end;
+        while (true) {
+            while (left <= end && data[left] < v)
+                left++;
+            while (data[right] > v)
+                right--;
+            if (left < right) {
+                int tmp = data[left];
+                data[left] = data[right];
+                data[right] = tmp;
+                left++;
+                right--;
+            } else
+                break;
+        }
+        data[start] = data[right];
+        data[right] = v;
+        quickSort(data, start, right-1);
+        quickSort(data, right+1, end);
+    }
     public static void main(String[] argv) {
-        int[] data = new int[]{5, 2, 7, 1, 8, 4, 9};
-        sort(data);
+        // int[] data = new int[]{5, 2, 7, 1, 8, 4, 9};
+        int[] data = new int[]{1,1,1,1,1,2,1,1,1};
+        quickSort(data);
         for (int i = 0; i < data.length; i++)
             System.out.println(data[i]);
     }
